508a80a2df12b5dcacaa459bc2ff11818ce1f312
Prarabdh Garg <prarabdhgrg@gmail.com>
2021-04-09 18:59:46 +0530

    Add command line script for searching

---
+++ Search_Apps/client.js
@@ -0,0 +1,97 @@
+'use strict';
+
+const { Gateway, Wallets } = require('fabric-network');
+const FabricCAServices = require('fabric-ca-client');
+const path = require('path');
+const { buildCAClient, registerAndEnrollUser, enrollAdmin } = require('../test-application/javascript/CAUtil.js');
+const { buildCCPOrg1, buildWallet } = require('../test-application/javascript/AppUtil.js');
+const prompt = require('prompt-sync')({sigint: true});
+
+
+const channelName = 'mychannel';
+const chaincodeName = 'basic';
+const mspOrg1 = 'Org1MSP';
+const walletPath = path.join(__dirname, 'wallet1');
+const org1UserId = 'appUserClient';
+var smartContract = null
+var ipfs = null
+
+async function main() {
+	// ipfs = await IPFS.create()
+	try {
+		// build an in memory object with the network configuration (also known as a connection profile)
+		const ccp = buildCCPOrg1();
+
+		// build an instance of the fabric ca services client based on
+		// the information in the network configuration
+		const caClient = buildCAClient(FabricCAServices, ccp, 'ca.org1.example.com');
+
+		// setup the wallet to hold the credentials of the application user
+		const wallet = await buildWallet(Wallets, walletPath);
+
+		// in a real application this would be done on an administrative flow, and only once
+		await enrollAdmin(caClient, wallet, mspOrg1);
+
+		// in a real application this would be done only when a new user was required to be added
+		// and would be part of an administrative flow
+		await registerAndEnrollUser(caClient, wallet, mspOrg1, org1UserId, 'org1.department2');
+
+		// Create a new gateway instance for interacting with the fabric network.
+		// In a real application this would be done as the backend server session is setup for
+		// a user that has been verified.
+		const gateway = new Gateway();
+
+		try {
+			// setup the gateway instance
+			// The user will now be able to create connections to the fabric network and be able to
+			// submit transactions and query. All transactions submitted by this gateway will be
+			// signed by this user using the credentials stored in the wallet.
+			await gateway.connect(ccp, {
+				wallet,
+				identity: org1UserId,
+				discovery: { enabled: true, asLocalhost: true } // using asLocalhost as this gateway is using a fabric network deployed locally
+			});
+
+			// Build a network instance based on the channel where the smart contract is deployed
+			const network = await gateway.getNetwork(channelName);
+
+			// Get the contract from the network.
+			smartContract = network.getContract(chaincodeName);
+
+			// const result = await smartContract.evaluateTransaction('ReadWord', 'word100')
+			// console.log('*** Result: committed ', result.toString());
+			var cont = true;
+			// while(cont) {
+				// console.log("Here")
+				cont = await foo()
+			// }
+		
+		} finally {
+			// Disconnect from the gateway when the application is closing
+			// This will close all connections to the network
+			// gateway.disconnect();
+		}
+
+
+	} catch (error) {
+		console.error(`******** FAILED to run the application: ${error}`);
+    }
+    
+    
+}
+
+
+async function foo() {
+    const searchWord = prompt('Enter search term: ');
+    console.log('Search term entered', searchWord);
+
+    var result = await smartContract.submitTransaction('Lookup', searchWord.toString())
+    console.log('Result from smart contract = ' + result.toString())
+
+	const cont = prompt('Continue?')
+	if(cont === 'y')
+		return true;
+	return false;
+}
+
+main();
